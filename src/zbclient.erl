1e801ec2fe0378022bbab3949d7b68a33bc159fc
deepthidevaki <deepthi.akkoorath@camunda.com>
2018-12-14 14:36:17 +0100

    can deploy and create workflow instance

--- src/zbclient.erl
+++ src/zbclient.erl
@@ -1,24 +1,73 @@
 -module(zbclient).
 
 %% API exports
--export([create_workflow_instance/2]).
+-export([create_workflow_instance/2,
+         create_workflow_instance/4,
+         deploy_workflow/3,
+         deploy_workflows/1,
+         get_version/1,
+         get_bpmn_process_id/1,
+         get_workflow_key/1,
+         get_workflows/1
+        ]).
+
+
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
-create_workflow_instance(WorkflowKey, BpmnProcessId) ->
+create_workflow_instance(WorkflowKey, BpmnProcessId, Version, Payload) ->
   Request =   #{workflowKey => WorkflowKey,
                 bpmnProcessId => BpmnProcessId,
-                version => -1},
+                version => Version,
+                payload => Payload},
   Response = gateway_protocol_gateway_client:create_workflow_instance(ctx:new(), Request),
   case Response of
-    {ok, Result,
-          _Metadata} ->
+    {ok, Result,_Metadata} ->
             Result;
     Error -> Error
   end.
 
+create_workflow_instance(WorkflowKey, BpmnProcessId) ->
+    create_workflow_instance(WorkflowKey, BpmnProcessId, -1, "").
+
+deploy_workflow(Name, Type, FilePath) ->
+    deploy_workflows([{Name, Type, FilePath}]).
+
+deploy_workflows(Workflows) ->
+    WorkflowObjects = lists:map(
+                        fun({Name, Type, FilePath}) ->
+                            {ok, Object} = get_workflow_object(Name, Type, FilePath),
+                            Object
+                        end, Workflows),
+    Request = #{workflows => WorkflowObjects},
+    Response = gateway_protocol_gateway_client:deploy_workflow(ctx:new(), Request),
+    case Response of
+        {ok, WorkflowMetadata, _GrpcMetadata} -> WorkflowMetadata;
+        Error -> Error
+    end.
+
+get_workflow_key(#{workflowKey := Value}) ->
+    {ok, Value}.
+
+get_bpmn_process_id(#{bpmnProcessId := Value}) ->
+    {ok, Value}.
+
+get_version(#{version := Value}) ->
+        {ok, Value}.
+
+get_workflows(#{workflows := Value}) ->
+        {ok, Value}.
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+get_workflow_object(Name, Type, FilePath) ->
+    case file:read_file(FilePath) of
+        {ok, Resource} ->
+            {ok, #{name => Name, type => Type, definition => Resource}};
+        {error, Reason} ->
+            {error_reading_file, Reason}
+    end.
--- test/client_test_SUITE.erl
+++ test/client_test_SUITE.erl
@@ -1,40 +1,63 @@
 -module(client_test_SUITE).
 
 %% common_test callbacks
--export([
-          init_per_suite/1,
+-export([ init_per_suite/1,
           end_per_suite/1,
           init_per_testcase/2,
           end_per_testcase/2,
-         all/0,
-         wf_instance_create_test/1
+          all/0
+        ]).
+
+%% Test cases
+-export([workflow_instance_create_test/1,
+         deploy_workflow_test/1
         ]).
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+%%Add all testcases here
 all() ->
- [wf_instance_create_test].
+ [workflow_instance_create_test,
+ deploy_workflow_test].
 
  init_per_suite(Config) ->
     application:load(grpcbox),
     application:set_env(grpcbox, client, #{channels => [{default_channel, [{http, "localhost", 26500, []}], #{}}]}),
     application:set_env(grpcbox, transport_opts, #{}),
-application:ensure_all_started(grpcbox),
+    application:ensure_all_started(grpcbox),
     Config.
 
  end_per_suite(Config) ->
      Config.
 
  init_per_testcase(_Case, Config) ->
-    application:set_env(grpcbox, client, #{channels => [{default_channel, [{http, "localhost", 26500, []}], #{}}]}),
     Config.
 
  end_per_testcase(_Name, _) ->
      ok.
 
-wf_instance_create_test(_Config) ->
+deploy_workflow_test(_Config) ->
+    Result = zbclient:deploy_workflow(
+                    "order-process",'BPMN',
+                    "/home/deepthi/work/zeebe/zeebe-broker-0.14.0/bin/order-process.bpmn"
+            ),
+    {ok, [Workflow]} = zbclient:get_workflows(Result),
+    {ok, ProcessId} = zbclient:get_bpmn_process_id(Workflow),
+    ?assertEqual(<<"order-process">>, ProcessId),
+    {ok, Version} = zbclient:get_version(Workflow),
+    ?assert(Version > 0),
+    {ok, WorkflowKey} = zbclient:get_workflow_key(Workflow),
+    CreateInstanceResponse = zbclient:create_workflow_instance(WorkflowKey, ProcessId, Version, "{\"orderId\": 12345}"),
+    ?assertNotMatch({error, _}, CreateInstanceResponse),
+    ?assertEqual({ok, ProcessId}, zbclient:get_bpmn_process_id(CreateInstanceResponse)),
+    ?assertEqual({ok, WorkflowKey}, zbclient:get_workflow_key(CreateInstanceResponse)),
+    ok.
+
+workflow_instance_create_test(_Config) ->
     Result = zbclient:create_workflow_instance(3, "test-process"),
-    #{bpmnProcessId := <<"test-process">>,version := _V,
-             workflowInstanceKey := _Key, workflowKey := 3} = Result,
+    %% #{bpmnProcessId := <<"test-process">>,version := _V,
+    %%         workflowInstanceKey := _Key, workflowKey := 3} = Result,
+    ?assertEqual({ok,<<"test-process">>}, zbclient:get_bpmn_process_id(Result)),
+    ?assertEqual({ok, 3}, zbclient:get_workflow_key(Result)),
     ok.
